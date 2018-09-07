889dc38e2c7fa3c549dd053631db62001e29d2f2
Noway <dpq@zjmax.com>
2018-09-07 11:38:27 +0800

    首次提交

---
+++ src/main/java/com/noway/wechatorder/WechatorderApplication.java
@@ -0,0 +1,12 @@
+package com.noway.wechatorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatorderApplication.class, args);
+    }
+}
